ee6b3a1d2cf589a6752ea14189971cf685945378
Hector Ramirez Garcia <hectorfx130@gmail.com>
2023-03-22 11:43:15 +0100

    Translate function implement

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { User } from './_models';
 import { AccountService } from './_services';
 import { JqueryService } from './_services/jquery.services';
-
+import {TranslateService} from "@ngx-translate/core";
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -14,13 +15,15 @@ export class AppComponent implements OnInit{
   user: User | null;
   startComments!: number;
 
-  constructor(private accountService: AccountService, private jqueryService: JqueryService) {
+  constructor(private accountService: AccountService, private jqueryService: JqueryService, private translate: TranslateService, private router: Router) {
     this.user = this.accountService.userValue;
+    translate.setDefaultLang('en');
+    translate.use('en');
   }
   
   title = 'DUO';
-  idioms = [ {id: 1, name: "es", text: "Es - Español"}, {id: 2, name: "en", text: "En - English"}]
-  comments = [ {id: 1, id_user: "1", text: "Buenarda la web!"}, {id: 2, id_user: "2", text: "Malisima la web!"}, {id: 3, id_user: "3", text: "Regular la web!"}]
+  idioms= [{id: "1", name: "en", text: "En - English"}, {id: "2", name: "es", text: "Es - Español"}];
+  comments = [{id: 1, id_user: "1", text: "Buenarda la web!"}, {id: 2, id_user: "2", text: "Malisima la web!"}, {id: 3, id_user: "3", text: "Regular la web!"}];
   isCollapsed = true;
 
   ngOnInit(): void {
@@ -29,7 +32,10 @@ export class AppComponent implements OnInit{
     this.startComments = 0;
   }
 
-  changeIdiom(){
+  changeIdiom(name: string){
+    console.log("loco")
+    this.translate.use(name);
+    this.router.navigate(['/']);
   }
 
   nextComments(){
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,17 @@ import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpClientModule, HttpClient, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { JwtInterceptor, ErrorInterceptor } from './_helpers';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home';
 import { InfiniteScrollModule } from 'ngx-infinite-scroll';
+
+//translate page imports
+import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
 
 @NgModule({
   declarations: [
@@ -23,7 +27,14 @@ import { InfiniteScrollModule } from 'ngx-infinite-scroll';
     HttpClientModule,
     AppRoutingModule,
     InfiniteScrollModule,
-    NgbModule
+    NgbModule,
+    TranslateModule.forRoot({
+      loader: {
+          provide: TranslateLoader,
+          useFactory: HttpLoaderFactory,
+          deps: [HttpClient]
+      }
+    }),
   ],
   providers: [ ApiriotgamesService,
     { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
@@ -34,3 +45,7 @@ import { InfiniteScrollModule } from 'ngx-infinite-scroll';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
+  return new TranslateHttpLoader(http);
+}
